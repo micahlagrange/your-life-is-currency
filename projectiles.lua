965215d519ea12903f41255641f61da9109dd330
Micah Turner <turner.micah@gmail.com>
2024-02-21 15:44:33 -0700

    wrong ice spike deleted on cleanup

--- projectiles.lua
+++ projectiles.lua
@@ -5,9 +5,14 @@ local bullets = {}
 local icePlatforms = {}
 
 BULLET_SPEED = 600
-local shootDelay = 1 -- Delay in seconds
+local shootDelay = 1  -- Delay in seconds
 local shootTimer = shootDelay
+local cleanDelay = 20 -- Delay in seconds
+local cleanTimer = cleanDelay
+local meltDelay = 10
 local image = love.graphics.newImage('sprites/ice-spike-1.png')
+
+local theWastelandOfDeadBullets = -9999
 
 
 -- Define the Projectile class
@@ -32,35 +37,7 @@ function Bullet.new(x, y, direction)
     self.collider:setGravityScale(0)
     self.collider:setFixedRotation(true)
     self.collider:setObject(self)
-    self.meltTimer = 10 -- seconds til melt
     return self
-end
-
-function NumBullets()
-    local size = 0
-    for _ in pairs(bullets) do size = size + 1 end
-    return size
-end
-
-function DeleteBullet(bullet, idx, melted)
-    local trackingTable = bullets
-    if melted then
-        trackingTable = icePlatforms
-    end
-
-    table.remove(trackingTable, idx)
-    bullet.collider:setCollisionClass('Ghost')
-    bullet.x = love.graphics.getWidth() / 2
-    bullet.y = love.graphics.getHeight() / 3
-    pcall(function()
-        bullet.collider:setX(bullet.x)
-        bullet.collider:setY(bullet.y)
-    end)
-end
-
-function MeltBullet(bullet, idx)
-    print('melt bullet ', idx)
-    DeleteBullet(bullet, idx, true)
 end
 
 local function checkCollisionWithWall(bullet, idx)
@@ -69,9 +46,85 @@ local function checkCollisionWithWall(bullet, idx)
         bullet.state = 'stuck'
         bullet.collider:setType('static')
         bullet.collider:setCollisionClass('Platform')
-        table.insert(icePlatforms, bullet)
-        table.remove(bullets, idx)
+        bullet.meltTimer = meltDelay
     end
+end
+
+local function move(dt)
+    -- Update existing bullets (move them forward)
+    for _, bullet in pairs(bullets) do
+        if bullet.state ~= 'stuck' then
+            if bullet.direction == LEFT then
+                bullet.x = bullet.x + dt * -BULLET_SPEED
+            else
+                bullet.x = bullet.x + dt * BULLET_SPEED
+            end
+            bullet.collider:setX(bullet.x)
+            bullet.collider:setY(bullet.y)
+
+            -- keep it from moving on y axis
+            local vx, _ = bullet.collider:getLinearVelocity()
+            bullet.collider:setLinearVelocity(vx, 0)
+
+            -- Check for collision with a wall (you'll need to implement this)
+            checkCollisionWithWall(bullet)
+        end
+    end
+end
+
+local function iceMelt(dt)
+    -- clean up static spikes
+    for i, spike in ipairs(bullets) do
+        if spike.state == 'static' then
+            spike.meltTimer = spike.meltTimer - dt
+            if spike.meltTimer <= 0 then
+                MeltBullet(spike, i)
+                return
+            end
+        end
+    end
+end
+
+local function cleanUpBullets(dt)
+    cleanTimer = cleanTimer - dt
+    if cleanTimer > 0 then return end
+
+    local colliders = World:queryCircleArea(theWastelandOfDeadBullets, theWastelandOfDeadBullets, 300)
+    for _, collider in ipairs(colliders) do
+        print('delete collider ', collider.id)
+        print(pcall(function() collider:destroy() end))
+    end
+    cleanTimer = cleanDelay
+end
+
+function Update(dt)
+    move(dt)
+    iceMelt(dt)
+    cleanUpBullets(dt)
+end
+
+function NumBullets()
+    local size = 0
+    for _ in pairs(bullets) do size = size + 1 end
+    return size
+end
+
+function DeleteBullet(bullet, idx)
+    local trackingTable = bullets
+
+    table.remove(trackingTable, idx)
+    bullet.collider:setCollisionClass('Ghost')
+    bullet.x = theWastelandOfDeadBullets
+    bullet.y = theWastelandOfDeadBullets
+    pcall(function()
+        bullet.collider:setX(bullet.x)
+        bullet.collider:setY(bullet.y)
+    end)
+end
+
+function MeltBullet(bullet, idx)
+    print('melt bullet ', idx)
+    DeleteBullet(bullet, idx)
 end
 
 function Shoot(dt, x, y, playerFacing)
@@ -104,59 +157,5 @@ function DrawBullets()
 end
 
 function SpawnBullet(x, y, direction)
-    -- if NumBullets() > 3 then
-    --     print('full array, delete first')
-    --     DeleteBullet(bullets[1], 1)
-    -- end
     table.insert(bullets, Bullet.new(x, y, direction))
-end
-
-function Update(dt)
-    -- Update existing bullets (move them forward)
-    for i, bullet in ipairs(bullets) do
-        if bullet.state == 'flying' then
-            if bullet.direction == LEFT then
-                bullet.x = bullet.x + dt * -BULLET_SPEED
-            else
-                bullet.x = bullet.x + dt * BULLET_SPEED
-            end
-            bullet.collider:setX(bullet.x)
-            bullet.collider:setY(bullet.y)
-
-            -- keep it from moving on y axis
-            local vx, _ = bullet.collider:getLinearVelocity()
-            bullet.collider:setLinearVelocity(vx, 0)
-
-            -- Remove bullets that go off-screen
-            if bullet.collider:getX() > love.graphics.getWidth() + Camera.x + 10 then
-                DeleteBullet(bullet, i)
-                return
-            end
-
-            -- Check for collision with a wall (you'll need to implement this)
-            checkCollisionWithWall(bullet)
-        end
-    end
-
-    for i, spike in ipairs(icePlatforms) do
-        spike.meltTimer = spike.meltTimer - dt
-        -- Delete melted bullets
-        if spike.meltTimer <= 0 then
-            MeltBullet(spike, i)
-            return
-        end
-    end
-end
-
-function CleanUpBullets()
-    for _, bulSpikes in pairs({ bullets, icePlatforms }) do
-        for _, bullet in ipairs(bulSpikes) do
-            if bullet.deleted then
-                local success, err = pcall(function() bullet.collider:destroy() end)
-                if not success then
-                    print(err)
-                end
-            end
-        end
-    end
 end
